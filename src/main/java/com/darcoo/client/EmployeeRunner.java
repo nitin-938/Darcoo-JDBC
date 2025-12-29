455c65143a603f2c9f5baad11d337a1c0bb6af3c
Nitin8795 <vishwakarmanitin987@gmail.com>
2025-12-29 11:51:41 +0530

    update

--- src/main/java/com/darcoo/client/EmployeeRunner.java
+++ src/main/java/com/darcoo/client/EmployeeRunner.java
@@ -1,11 +1,7 @@
  package com.darcoo.client;
- 
   
-
-
- 
+import java.sql.Connection;
 import java.sql.DriverManager;
-  
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
@@ -17,20 +13,23 @@ public class EmployeeRunner {
 	public static void main(String[]args) throws SQLException {
 		
 //      Persistent Object
-		Employee ravi = new Employee(9,"Aman", "male",92000 );
+		Employee ravi = new Employee(10,"Aman", "male",92000 );
 		
-//		Properties ps = new Properties();
-//		
-//		ps.put("user", "root");
-//		ps.put("password", "root");
-//		
-//		Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", ps);
-//		
-//		Statement statement = connection.createStatement();
-//		
-//     	statement.executeUpdate("insert into employee(id,name,gender,salary)value ("+ravi.getId()+ ",'"+ravi.getName()+"','"+ravi.getGender()+"',"+ravi.getSalary()+")");
-//		
-//		
+		
+//		way-2
+		
+		Properties ps = new Properties();
+		ps.put("user", "root");
+		ps.put("password", "root");
+		
+		
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", ps);
+		
+		
+	    Statement statement = connection.createStatement();
+		
+    	statement.executeUpdate("insert into employee(id,name,gender,salary)value ("+ravi.getId()+ ",'"+ravi.getName()+"','"+ravi.getGender()+"',"+ravi.getSalary()+")");
+	
 		
 //		Multiple method  connection
 		/*
@@ -44,7 +43,7 @@ public class EmployeeRunner {
 		
 //		Method Chaining (in single method) connection
 		
-		DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root").createStatement().executeUpdate("insert into employee(id,name,gender,salary)value ("+ravi.getId()+ ",'"+ravi.getName()+"','"+ravi.getGender()+"',"+ravi.getSalary()+")");
+//		DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root").createStatement().executeUpdate("insert into employee(id,name,gender,salary)value ("+ravi.getId()+ ",'"+ravi.getName()+"','"+ravi.getGender()+"',"+ravi.getSalary()+")");
 		 
 		
 		 
